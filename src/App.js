3a29f37167b2f0c2b867bd660de59d60546455d6
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-06-07 15:19:24 -0300

    1 ao 6 feitos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/users">
+            Users
+          </Link>
+        </li>
+      </ul>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/users" component={Users}/>
+      </BrowserRouter>
     );
   }
 }
